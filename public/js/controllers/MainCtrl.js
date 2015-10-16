14e801eade7827fbcdb669e9af18e5a075b6f7a7
Sean Darcy <darcys22@gmail.com>
2015-10-16 23:10:48 +1100

    ui kinda looks good

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,24 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.tagline = 'To the moon and back!';	
+  $scope.open = function($event) {
+    $scope.status[$event].opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.format = 'dd-MMMM-yyyy'
+
+  $scope.status = [
+  {
+    opened: false
+  },
+  {
+    opened: false
+  }
+  ];
 
 });
